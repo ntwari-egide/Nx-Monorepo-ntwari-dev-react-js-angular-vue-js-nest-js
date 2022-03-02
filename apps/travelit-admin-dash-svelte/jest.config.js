6361d62a207c74903a2890e2baa0b8fdb3cbf4d0
ntwari-egide <ntwariegide2@gmail.com>
2022-03-02 05:34:15 +0200

    ft(admin-dash): add svelte admin dashboard

---
+++ apps/travelit-admin-dash-svelte/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  displayName: 'travelit-admin-dash-svelte',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^(.+\\.svelte$)': [
+      'svelte-jester',
+      {
+        preprocess: 'apps/travelit-admin-dash-svelte/svelte.config.cjs',
+      },
+    ],
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['svelte', 'ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/travelit-admin-dash-svelte',
+};
