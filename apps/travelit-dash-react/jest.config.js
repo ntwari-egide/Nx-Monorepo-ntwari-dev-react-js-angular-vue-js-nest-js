ec304107eb7db092f44c695ed4703cbc872cb76f
ntwari-egide <ntwariegide2@gmail.com>
2022-03-02 04:50:07 +0200

    ft(dashb): add react to nx workspace

---
+++ apps/travelit-dash-react/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'travelit-dash-react',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/travelit-dash-react',
+};
