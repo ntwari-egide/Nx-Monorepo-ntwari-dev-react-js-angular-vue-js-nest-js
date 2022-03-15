0c4debc57c064ff0e1d598e326441019ee3787cb
ntwari-egide <ntwariegide2@gmail.com>
2022-03-15 07:03:41 +0200

    ft(shora-api): implement api endpoint

---
+++ apps/shora-api/src/main/java/com/shora/backend/ShoraApiApplication.java
@@ -0,0 +1,13 @@
+package com.shora.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoraApiApplication.class, args);
+	}
+
+}
---
+++ apps/shora-api/src/test/java/com/shora/backend/ShoraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shora.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
